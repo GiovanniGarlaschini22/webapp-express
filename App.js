66675c2748c867c8e211981c241c1533c4c5717d
GiovanniGarlaschini22 <giovannigarlaschini.boolean@gmail.com>
2025-06-26 16:39:46 +0200

    Feat: database collegato, API testata

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import express from "express";
+import connection from "./db.js";
 
 const app = express();
 const port = process.env.SERVER_PORT;
 
 app.use(express.json());
 app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.json({
+        data: "welcome to Movies API"
+    });
+});
 
 app.listen(port, () => {
     console.log(`application is listening on port ${port}`);  
